bcfa230900646dea41f3acadb57b54e4ae80c10a
Nam Huynh <akirathanhnam@gmail.com>
2020-11-19 23:05:08 +0700

    remove + in not found search result

--- webapp/blog/templatetags/custom_tags.py
+++ webapp/blog/templatetags/custom_tags.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter
 def replace_quotes(value):
     return value.replace("'", '"')
+
+@register.filter
+def replace_plus(value):
+    return value.replace("+", " ")
